21bfb9f7c6de7f29e06cf4c7fb8608de3400e9ee
pedroleandro <pedro.leandrog@gmail.com>
2020-07-07 00:13:34 -0300

    desenvolvendo aplicação

--- app/Email.php
+++ app/Email.php
@@ -3,16 +3,46 @@
 namespace Notification\Email;
 
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
 class Email
 {
+    private $mail = \stdClass::class;
+
     public function __construct()
     {
-        $email = new PHPMailer();
+        $this->mail = new PHPMailer(true);
+        //Server settings
+        $this->mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+        $this->mail->isSMTP();                                            // Send using SMTP
+        $this->mail->Host = 'smtp.gmail.com';                    // Set the SMTP server to send through
+        $this->mail->SMTPAuth = true;                                   // Enable SMTP authentication
+        $this->mail->Username = 'developerfsphp@gmail.com';                     // SMTP username
+        $this->mail->Password = '9cPA8!Vhd3H*njBn';                               // SMTP password
+        $this->mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $this->mail->Port = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+        $this->mail->CharSet = "utf-8";
+        $this->mail->setLanguage("br");
+        $this->mail->isHTML(true);
+
+        //Recipients
+        $this->mail->setFrom('developerfsphp@gmail.com', 'Team Developer');
     }
 
-    public function send()
+    public function send($subject, $body, $replyEmail, $replyName, $addressEmail, $addressName)
     {
-        echo "E-mail enviado com sucesso!";
+        $this->mail->Subject = (string)$subject;
+        $this->mail->Body = $body;
+
+        $this->mail->addReplyTo($replyEmail, $replyName);
+        $this->mail->addAddress($addressEmail, $addressName);
+
+        try {
+            $this->mail->send();
+        } catch (Exception $exception) {
+            echo "Erro ao enviar e-mail: {$this->mail->ErrorInfo} {$exception->getMessage()}";
+        }
     }
 }
--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 
-require __DIR__."/vendor/autoload.php";
+require __DIR__ . "/vendor/autoload.php";
 
 use Notification\Email\Email;
 
 $newEmail = new Email();
 
-$newEmail->send();
+$newEmail->send("Testando PHPMailer", "<p>Olá, Pedro!</p>",
+                "developerfsphp@gmail.com", "teamdeveloper",
+                "pedro.leandrog@gmail.com", "Pedro Leandro");
+
+var_dump($newEmail);
